b197483b4deb9a4cdf2c7b6a56d1571d447afdb9
tamph973 <phtam0907@gmail.com>
2023-08-28 00:02:40 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const contactsRouter = require("./app/routes/contact.route");
+app.use("/api/contacts", contactsRouter);
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
 
+
 module.exports = app;
+
